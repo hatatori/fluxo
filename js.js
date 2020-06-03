e3aca26a663534f057d54c07bb0f7bceecb608e8
Hatatori Nitsukawa <kcireh.i@gmail.com>
2020-06-02 22:58:43 -0300

    Add files via upload

--- js.js
+++ js.js
@@ -1,126 +1,182 @@
-function encontra_ul(a,b){
-	    return a.querySelectorAll("ul")[b]
-	}
+function jogaPraCima(n){
+	try{
+		u.children[n-1].querySelector('ul').appendChild(u.children[n])
+	}catch(e){}
+}
 
-	function joga_pra_cima(n){
-	    u2.children[n-1].querySelector('ul').appendChild(u2.children[n])
-	}
+function jogaProUltimo(n){
+	try{
+		ultimo = u.children[n-1]
 
-	function joga(a,b){
-	    a.appendChild(b)
+		ultimo.querySelectorAll('ul')
+
+		len = ultimo.querySelectorAll('ul').length-1
+
+		kz = ultimo.querySelectorAll('ul')[len]
+
+		kz.appendChild(u.children[n])
+	}
+	catch(e){}
+}
+
+
+function getName(n){
+	return u.children[n].getAttribute('name')
+}
+
+function getP(n){
+	return parseInt(u.children[n].getAttribute('p'))
+}
+
+	// function return_ul(n){
+	// 	return u.querySelectorAll("ul")[n]
+	// }
+
+	function return_ul_in(n){
+		return parseInt(u.querySelectorAll("ul")[n].getAttribute('u'))
 	}
 	
-	function go(linha){
 
-		function tab(n){ try { return n.split("\t").length-1}catch(e){return 0} }
+	//retorna quantidade de tab na linha n
+	function tab(n){
+		try{
+			return parseInt(linha[n].match(/\t/g).length)
+		}catch(e){
+			return 0
+		}
+	}
+	
 
-		linha = linha.split("\n")
+	//cria todo o esquema
+	function cria(a){
+		t = ""
 
-		ar = linha.map(e=>"<li>"+e.replace(/\t/g,"")+"</li>")
+		// linha = txt.value.split("\n");
+		linha = a.split("\n")
 
-		lista = []
-		s = 0
-
-
-		u2 = document.createElement("ul")
+		u = document.createElement("ul")
 
 		for(i=0;i<linha.length;i++){
-			nome = linha[i].replace(/\t/g,"")
-			lista.push(nome)
-		}
 
-		for(i=0;i<lista.length;i++){
-			if(tab(linha[i+1]) > tab(linha[i]))
-				lista[i] = "<li class='pos' p="+tab(linha[i])+"><span class='tf-nc'>"+lista[i]+"</span><ul pos='"+tab(linha[i+1])+"'></ul></li>"
+			maior = tab(i+1) > tab(i)
+			col = tab(i)
+
+
+			linha[i] = linha[i].replace(/\t/g,"")
+
+
+			if(maior)
+				t += "<li name='a' p="+col+"><span class='tf-nc'>"+linha[i]+"</span><ul u="+col+"></ul></li>"
 			else
-				lista[i] = "<li class='val' p="+tab(linha[i])+"><span class='tf-nc'>"+lista[i]+"</span></li>"
-			
+				t += "<li name='c' p="+col+"><span class='tf-nc'>"+linha[i]+"</span></li>"
 		}
 
-		u2 = document.createElement("ul")
-		u2.innerHTML = lista.join("")
-
-		function organiza(){
-
-			for(x=0;x<10;x++){
-				ql = u2.querySelectorAll("li")
-				for(i=u2.childElementCount;i>=0;i--){
-					try{
-
-						p1 = u2.children[i-1].getAttribute("p")
-						p2 = u2.children[i].getAttribute("p")
-
-						nome1 = u2.children[i-1].getAttribute("class")
-						nome2 = u2.children[i].getAttribute("class")
-
-						filho = u2.children
-
-						if(u2.children[i].className == "val" && p1 < p2)
-							joga(filho[i-1].querySelector('ul'),filho[i])
-
-					}catch(e){}
-				}
-			}
-		}
-
-		function organiza2(){
-			
-			for(i=u2.childElementCount;i>=0;i--){
-
-				try{
-
-					p1 = u2.children[i-1].getAttribute("p")
-					p2 = u2.children[i].getAttribute("p")
-					
-					nome1 = u2.children[i-1].getAttribute("class")
-					nome2 = u2.children[i].getAttribute("class")
-
-					nin = u2.children[i-1].querySelector('ul').getAttribute("pos")
-
-					filho = u2.children
-
-					if(p2 > p1 && p2 == nin){
-
-
-						joga(filho[i-1].querySelector('ul'),filho[i])
-						
-						filho = u2.children
-
-
-						get1 = parseInt(filho[i].getAttribute("p"))
-						get2 = parseInt(filho[i-1].getAttribute("pos"))
-
-
-						if(get1 == filho[i-1].querySelectorAll('ul')[0].getAttribute("pos")){
-							joga(filho[i-1].querySelectorAll('ul')[0],filho[i])
-							joga(filho[i-1].querySelectorAll('ul')[0],filho[i])
-						}
-					}
-
-				}catch(e){}
-
-			}
-		}
-
-		organiza()
-
-		for(ti=0;ti<10;ti++){
-			organiza2()
-		}
-
-
-
-
-	return u2.outerHTML;
-
+		u.innerHTML = t
+	}
 	
-}
+
+	// ajusta o esquema
+
+	function organiza1(){
+		for( i = linha.length-1 ;  i >= 0 ;i--){
+			try{
+
+				igual = getP(i) == getP(i-1)
+				menor = getP(i) < getP(i-1)
+				maior = getP(i) > getP(i-1)
+
+				nome_igual = getP(i)==getP(i-1)
+				nome_diferente = getP(i)!=getP(i-1)
+
+
+				if(maior && nome_diferente && return_ul_in(i-1) < getP(i))
+					jogaPraCima(i)
+
+
+			}catch(e){}
+
+		}
+	}
+
+	function organiza2(){
+		for( i = linha.length-1 ;  i >= 0 ;i--){
+			try{
+
+				igual = getP(i) == getP(i-1)
+				menor = getP(i) < getP(i-1)
+				maior = getP(i) > getP(i-1)
+
+				nome_igual = getP(i)==getP(i-1)
+				nome_diferente = getP(i)!=getP(i-1)
+
+
+				if(maior && nome_diferente && return_ul_in(i-1) < getP(i)){
+					jogaProUltimo(i)
+				}
+
+
+			}catch(e){}
+
+		}
+	}
+
+	function organiza3(){
+		
+		li = u.querySelectorAll("li")
+
+		try{
+
+			for( i = li.length-1 ;  i >= 0 ;i--){	
+
+				numero_pai = parseInt(u.querySelectorAll("li")[i].parentElement.getAttribute("u"))
+				numero_avo = parseInt(u.querySelectorAll("li")[i].parentElement.getAttribute("p"))
+				numero = parseInt(u.querySelectorAll("li")[i].getAttribute("p"))
+
+				if(numero <= numero_pai || numero <= numero_avo)
+					u.querySelectorAll("li")[i].parentElement.parentElement.appendChild(u.querySelectorAll("li")[i])
+			}
+		}catch(e){}
+
+	}
+
+
+	//modela 
+	
+	function render(n){
+
+		cria(n)
+		
+		organiza1()
+
+		organiza2()
+		organiza2()
+		organiza2()
+		organiza2()
+		organiza2()
+		organiza2()
+		organiza2()
+
+		organiza3()
+		organiza3()
+		organiza3()
+		organiza3()
+		organiza3()
+		organiza3()
+		organiza3()
+		organiza3()
+
+
+		ok.innerHTML = u.outerHTML
+
+		return u.outerHTML
+	}
+
 
 // go()
 
 
-	d1 = cont.innerHTML.split("\n\n\n")
+d1 = cont.innerHTML.split("\n\n\n")
 
-	for(i of d1){
-		cont2.innerHTML += "<div class='tf-tree example'>"+go(i)+"</div>"
-	}
+for(i of d1){
+	cont2.innerHTML += "<div class='tf-tree example'>"+render(i)+"</div>"
+}
